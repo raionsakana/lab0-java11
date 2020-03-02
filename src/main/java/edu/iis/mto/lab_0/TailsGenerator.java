0379ef99681cb8b00227915f1e72899f23b8d32c
janowski.patryk <215745@edu.p.lodz.pl>
2020-03-02 18:59:18 +0100

    code formating added

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,12 +6,12 @@ import java.util.List;
 class TailsGenerator {
 
     static List<String> tails(String value) {
-
         ArrayList<String> list = new ArrayList<>();
         int sizeOfValue = value.length();
 
-        for (int i = 0; i < sizeOfValue + 1; i++)
+        for (int i = 0; i < sizeOfValue + 1; i++) {
             list.add(value.substring(i, sizeOfValue));
+        }
 
         return list;
     }
--- src/test/java/edu/iis/mto/lab_0/TailsGeneratorTest.java
+++ src/test/java/edu/iis/mto/lab_0/TailsGeneratorTest.java
@@ -1,12 +1,12 @@
 package edu.iis.mto.lab_0;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import java.util.List;
-
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class TailsGeneratorTest {
 
